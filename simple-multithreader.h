49231072767896083e5fa373d2f958af35010ef1
Shrutya <131897020+shrutya22487@users.noreply.github.com>
2023-11-16 21:40:37 +0530

    Update simple-multithreader.h

--- simple-multithreader.h
+++ simple-multithreader.h
@@ -11,6 +11,16 @@
 
 int user_main(int argc, char **argv);
 
+long get_time(){
+    struct timeval time, *address_time = &time;
+    if (gettimeofday(address_time, NULL) != 0) {
+        perror("Error in printing the time.");
+        exit(1);
+    }
+    long epoch_time = time.tv_sec * 1000;
+    return epoch_time + time.tv_usec / 1000;
+}
+
 typedef struct {
     int low, high;
     std::function<void (int)> lambda1D;
@@ -23,7 +33,7 @@ typedef struct {
 
 void *thread_func_1D(void *ptr) {
     thread_args_1D * t = ((thread_args_1D *) ptr);
-    for (int i = t->low; i <= t->high; i++)
+    for (int i = t->low; i < t->high; i++)
     {
         t->lambda1D(i);
     }
@@ -32,42 +42,35 @@ void *thread_func_1D(void *ptr) {
 
 void *thread_func_2D(void *ptr) {
     thread_args_2D * t = ((thread_args_2D *) ptr);
-    for (int i = t->low1; i <= t->high1; i++)
+
+    for (int i = t->low1; i < t->high1; i++)
     {
-        for (int j = t->low2; j <= t->high2; j++){
+        for (int j = t->low2; j < t->high2; j++){
             t->lambda2D(i, j);
         }    
     }
     return NULL;
 }
 
-long get_time(){
-    struct timeval time, *address_time = &time;
-    if (gettimeofday(address_time, NULL) != 0) {
-        perror("Error in printing the time.");
-        exit(1);
-    }
-    long epoch_time = time.tv_sec * 1000;
-    return epoch_time + time.tv_usec / 1000;
-}
-
 void parallel_for(int low, int high, std::function<void(int)> &&lambda, int NTHREADS) {
+    
+    long start_time=get_time();
     pthread_t tid[NTHREADS];
     thread_args_1D args[NTHREADS];
     int chunk = (high - low )/NTHREADS;
 
-    long start_time=get_time();
-
     for (int i=0; i<NTHREADS; i++) {
         args[i].low=i*chunk; 
-        args[i].high = (i+1)*chunk;        
+        args[i].high = (i+1)*chunk;       
+        if (i == NTHREADS - 1) {
+            args[i].high = high; 
+        }    
         args[i].lambda1D = lambda;
         pthread_create(&tid[i],NULL,thread_func_1D,(void*) &args[i]);
     }
     for (int i=0; i<NTHREADS; i++) {
         pthread_join(tid[i] , NULL);
     }
-
     long end_time=get_time();
 
     long t1 = end_time-start_time;
@@ -75,31 +78,32 @@ void parallel_for(int low, int high, std::function<void(int)> &&lambda, int NTHR
 }
 
 void parallel_for(int low1, int high1, int low2, int high2, std::function<void(int, int)> &&lambda, int NTHREADS){
+    long start_time=get_time();
     pthread_t tid[NTHREADS];
     thread_args_2D args[NTHREADS];
     int chunk1 = (high1 - low1 )/NTHREADS;
-    int chunk2 = (high2 - low2) / NTHREADS;
-
-    long start_time=get_time();
+    //int chunk2 = (high2 - low2) / NTHREADS;
 
     for (int i=0; i<NTHREADS; i++) {
+
         args[i].low1 =i*chunk1; 
         args[i].high1 = (i+1)*chunk1;
-
-        args[i].low2 =i*chunk2; 
-        args[i].high2 = (i+1)*chunk2;  
-                
+        args[i].low2 = 0; 
+        args[i].high2 = high1;  
+        if (i == NTHREADS - 1) {
+            args[i].high1 = high1; 
+        }
         args[i].lambda2D = lambda;
         pthread_create(&tid[i],NULL,thread_func_2D,(void*) &args[i]);
+
     }
+
     for (int i=0; i<NTHREADS; i++) {
         pthread_join(tid[i] , NULL);
     }
-
     long end_time=get_time();
     long t2=end_time-start_time;
     printf("Total execution time for matrix: %ld\n", t2);
-
 }
 int main(int argc, char **argv) {
   int rc = user_main(argc, argv);
@@ -107,3 +111,5 @@ int main(int argc, char **argv) {
 }
 
 #define main user_main
+
+
